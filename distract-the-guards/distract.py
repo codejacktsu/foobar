85b68b817dd998c7aea4c3dfae007f157826ccdb
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2021-01-11 16:30:48 -0800

    Update distract.py

--- distract-the-guards/distract.py
+++ distract-the-guards/distract.py
@@ -1,35 +1,6 @@
-import time
 # from fractions import gcd
 from math import gcd
-
-
-def timer(func):
-    """decorator: prints process runtime"""
-
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter() - start_time
-        print(f"Finished {func.__name__!r} in {end_time:.4f} secs")
-        return result
-    return wrapper
-
-
-class Memoize:
-    def __init__(self, function):
-        self.cache = {}
-        self.function = function
-
-    def __call__(self, *args, **kwargs):
-        key = str(args) + str(kwargs)
-        if key in self.cache:
-            print("memoized!")
-            return self.cache[key]
-
-        value = self.function(*args, **kwargs)
-        print("key added: ", key, " ", value)
-        self.cache[key] = value
-        return value
+from util import timer, Memoize
 
 
 def create_graph(lst):
@@ -111,6 +82,7 @@ def simple_form(pair):
     pair[0] = pair[0] // d
     pair[1] = pair[1] // d
     return pair
+
 
 @timer
 def solution(lst):
