4fff3e70f8c5c95c4bc03f0aa6f53902cf328fe2
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2021-01-25 16:04:23 -0800

    running v1 v2

--- running-with-bunnies/runningv2.py
+++ running-with-bunnies/runningv2.py
@@ -1,22 +1,18 @@
 from util import timer, Memoize
 
 
-#brute force
-
 def solution(times, time_limit):
-    return [bellman_ford(times, layer) for layer, _ in enumerate(times)]
+   pass
 
-def bellman_ford(cost, origin):
-    """ Use Bellman-Ford algorithm to find shortest path"""
 
-    graph = []
-    for i in range(len(cost)):
-        if i == origin:
-            graph.append(0)
-        else:
-            graph.append('inf')
+class Vertex:
+    def __init__(self, idx):
+        self.idx = idx
+        self.bunnies = []
+
+
+def solver(graph):
     for z in range(len(graph)-1):
-        beg_graph = graph.copy()
         for start, total_cost in enumerate(graph):
             if total_cost == 'inf':
                 continue
@@ -26,9 +22,8 @@ def bellman_ford(cost, origin):
                     graph[end] = delta
                 else:
                     graph[end] = min(graph[end], graph[start] + delta)
-        if graph == beg_graph:
-            break
-    return graph
+
+
 
 
 # times = [[0, 1, 1, 1, 1],
@@ -46,4 +41,3 @@ times = [[0, 2, 2, 2, -1],
 time_limit = 1
 
 print(solution(times, time_limit))
-# print(bellman_ford(times, 0))
